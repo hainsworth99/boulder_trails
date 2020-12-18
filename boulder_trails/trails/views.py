57920afe4b99d65fba417de0178b7de69a2931c1
hainsworth99 <31012794+hainsworth99@users.noreply.github.com>
2020-12-18 14:46:42 -0700

    added basic API endpoint to retrieve trail data

--- boulder_trails/trails/views.py
+++ boulder_trails/trails/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.core.serializers import serialize
+from django.http import HttpResponse
+from .models import Trail, TrailSegment, TrailClosure
 
-# Create your views here.
+def trails(request):
+
+    return HttpResponse(serialize('geojson', TrailSegment.objects.all()))
+
